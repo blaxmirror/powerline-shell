798af6318c47e1a545c75f2d044fc1655cf95e0a
Jialun Zhao <jialun.zhao@ericsson.com>
2018-04-16 10:33:22 +0800

    fix a bug that will crash when gsnws not matched
    Change-Id: Ia9e789e0ff32b88dec5e70638108950f5b04ef69

--- powerline_shell/segments/gsnws.py
+++ powerline_shell/segments/gsnws.py
@@ -4,12 +4,12 @@ from ..utils import BasicSegment
 
 def get_gsnws_name():
     pat_match = re.match('^\w{7}__ndpgsn_5_0_(?:wb__ndpgsn_5_0_(\w+)|(\w+))$', os.getenv('GSN_WS_NAME'))
+    if not pat_match:
+        return ''
     if pat_match.group(1):
         return pat_match.group(1)
     if pat_match.group(2):
         return pat_match.group(2)
-    return gsnws
-
 
 class Segment(BasicSegment):
     def add_to_powerline(self):
